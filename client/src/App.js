b027c168aba75b8c390148a359e76c2fc69e47cf
Mandie Kramer <37544463+ImpunityJainne@users.noreply.github.com>
2018-10-02 11:25:45 -0500

    added routing to the Register component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 const App = () => (
@@ -11,9 +12,10 @@ const App = () => (
       <Navbar />
 
       <Route exact path="/" component={Login} />
-      {/* <Route exact path="/profile" component={Profile} />
-      <Route exact path="/journal" component={Journal} />
-      <Route exact path="/settings" component={Settings} /> */}
+      <Route exact path="/register" component={Register} />
+      {/* <Route exact path="/profile" component={Profile} /> */}
+      {/* <Route exact path="/journal" component={Journal} /> */}
+      {/* <Route exact path="/settings" component={Settings} /> */}
 
     </div>
   </Router>
