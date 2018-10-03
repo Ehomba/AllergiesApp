b489eea0e428c17057895a50123b9b1a12235038
Spencer Vigil <spencervigil27@gmail.com>
2018-10-03 14:41:16 -0500

    auth0

---
+++ client/src/Auth/Auth.js
@@ -0,0 +1,65 @@
+// src/Auth/Auth.js
+
+import auth0 from 'auth0-js';
+import history from '../history';
+
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'YOUR_AUTH0_DOMAIN',
+    clientID: 'YOUR_CLIENT_ID',
+    redirectUri: 'http://localhost:3000/callback',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+
+  login() {
+    this.auth0.authorize();
+  }
+  constructor() {
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+  }
+  // handleAuthentication..looks for the result of authentication in the URL hash. Then, the result is processed with the parseHash method from auth0.js
+  handleAuthentication() {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace('/home');
+      } else if (err) {
+        history.replace('/home');
+        console.log(err);
+      }
+    });
+  }
+  //setSession..sets the user's Access Token, ID Token, and the Access Token's expiry time
+  setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/home');
+  }
+  //logout: removes the user's tokens and expiry time from browser storage
+  logout() {
+    // Clear Access Token and ID Token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+  //isAuthenticated: checks whether the expiry time for the user's Access Token has passed
+  isAuthenticated() {
+    // Check whether the current time is past the 
+    // Access Token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
+
+
+}
+
