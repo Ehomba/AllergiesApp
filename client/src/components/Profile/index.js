982f72793a339ebf013ad0072ef9d20967587c44
JaredWalker <jaredstaires@gmail.com>
2018-10-05 11:02:32 -0500

    Settings pagemove

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-
+import Settings from '../Settings';
 class Profile extends React.Component {
 
   render() {
       return (
-    
         <div>
-
-          <div className="row">
+        <Settings />
+                
+             
+              <div className="row">
 
             <div className="col s12 m6">
-        
+
               <div className="card blue-grey darken-1">
 
                 <div className="card-content white-text">
@@ -20,30 +20,17 @@ class Profile extends React.Component {
                 </div>
 
                 <div className="card-action">
-                  <Link to="">This is a link</Link>
-                  <Link to="">This is a link</Link>
+                 {/* <a href=""></a> */}
+                  {/* <a href="#"></a> */}
+                </div>
+                <div className="row">
                 </div>
                 
-              </div> {/* end of card */}
-            </div> {/* end of column */}
-
-            <div className="col s12 m6">
-
-              <div className="card blue-grey darken-1">
-
-                <div className="card-content white-text">
-                  <span className="card-title"></span>
-                  <p></p>
-                </div>
-
-                <div className="card-action">
-                  <Link to="">This is a link</Link>
-                  <Link to="">This is a link</Link>
-                </div>
 
               </div> {/* end of card */}
+
             </div> {/* end of column */}
-        
+
           </div> {/* end of end of row */}
 
         </div>
